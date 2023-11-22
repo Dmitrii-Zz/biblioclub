aa2fc685c224ddf70afd5f1bdaf2da1707be663d
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-11-22 18:44:49 +0700

    feat: артефакт Spring Boot

---
+++ src/main/java/ru/dynamika/biblioclub/BiblioclubApplication.java
@@ -0,0 +1,13 @@
+package ru.dynamika.biblioclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dynamika/biblioclub/BiblioclubApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dynamika.biblioclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

da8bbe65f5d0f895e49f459e3d5353f6b7160025
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-11-22 20:37:55 +0700

    feat: добавить выозвы методо сервиса юзера

--- src/main/java/ru/dynamika/biblioclub/user/controller/UserController.java
+++ src/main/java/ru/dynamika/biblioclub/user/controller/UserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.dynamika.biblioclub.user.model.User;
+import ru.dynamika.biblioclub.user.service.UserService;
 import ru.dynamika.biblioclub.user.service.UserServiceImpl;
 
 import java.util.List;
@@ -15,23 +16,23 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
-    private UserServiceImpl userService;
+    private UserService service;
 
     @PostMapping
     public User createUser(@Valid @RequestBody User user) {
         log.info("Запрос на создание юзера: " + user.getName());
-        return null;
+        return service.createUser(user);
     }
 
     @PatchMapping("/{userId}")
     public User updateUser(@Valid @RequestBody User user, @PathVariable long userId) {
         log.info("Запрос на обновление юзера: " + userId);
-        return null;
+        return service.updateUser(user, userId);
     }
 
     @GetMapping
     public List<User> getAllUser() {
         log.info("Запрос всех пользователей");
-        return null;
+        return service.getAllUsers();
     }
 }

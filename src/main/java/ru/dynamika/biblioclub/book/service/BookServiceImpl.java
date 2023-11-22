ec77b2d02b65cbcad140e5fa8fdaf7d649717ee0
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-11-22 21:56:08 +0700

    feat: добавить обновление книги

--- src/main/java/ru/dynamika/biblioclub/book/service/BookServiceImpl.java
+++ src/main/java/ru/dynamika/biblioclub/book/service/BookServiceImpl.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.dynamika.biblioclub.book.model.Book;
 import ru.dynamika.biblioclub.book.repository.BookRepository;
+import ru.dynamika.biblioclub.exceptions.ex.BookNotFoundException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,11 +21,35 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Book updateBook(Book book, long bookId) {
-        return null;
+        Book updateBook = checkExistsBook(bookId);
+
+        if (book.getName() != null) {
+            updateBook.setName(book.getName());
+        }
+
+        if (book.getAuthor() != null) {
+            updateBook.setAuthor(book.getAuthor());
+        }
+
+        if (book.getISBN() != null) {
+            updateBook.setISBN(book.getISBN());
+        }
+
+        return bookStorage.save(updateBook);
     }
 
     @Override
     public List<Book> getAllBook() {
-        return null;
+        return bookStorage.findAll();
+    }
+
+    private Book checkExistsBook(long bookId) {
+        Optional<Book> optBook = bookStorage.findById(bookId);
+
+        if (optBook.isEmpty()) {
+            throw new BookNotFoundException("Отсутствует книга с id = " + bookId);
+        }
+
+        return optBook.get();
     }
 }
